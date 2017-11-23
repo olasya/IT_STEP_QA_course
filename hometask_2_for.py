9cc9bc9728122a86ef593425ba35884f6a4cb2a0
Olga Hryhorieva <olasya@gmail.com>
2017-11-23 10:08:39 +0200

    hometask_2_for.py_task1_first_letters

--- hometask_2_for.py
+++ hometask_2_for.py
@@ -1,5 +1,7 @@
 names = ['Ann', 'Fillip', 'Monika', 'Ross', 'Sam', 'Alf', 'Roy', 'Sasha', 'Leon']
-for x in names:
-	print x
+# for x in names:
+# 	print x
 # for x in names:
 # 	print names.index(x)
+for x in names:
+	print x[0]
