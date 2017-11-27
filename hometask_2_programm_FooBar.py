9eaf46726a7515d448bc160f326471a7742922f7
Olga Hryhorieva <olasya@gmail.com>
2017-11-27 23:50:47 +0200

    hometask_2_programm_FooBar_add_null.py

--- hometask_2_programm_FooBar.py
+++ hometask_2_programm_FooBar.py
@@ -1,9 +1,14 @@
+
 a= int(input("Input number:"))
-if a%3==0 and a%5==0:
+
+if a <= 0:
+	print "Input positive number"
+elif a % 3 == 0 and a % 5 == 0:
  	print "FooBar"
-elif a%3==0:
+elif a % 3 == 0:
 	print "Foo"
-elif a%5==0:
+elif a % 5 == 0:
 	print "Bar"
 else:
  	print "GoodBy"	
+
